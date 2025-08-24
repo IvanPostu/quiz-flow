eed1fefeffe4318465e166625b1693df9c8c2a04
IvanPostu <ipostu20000127@gmail.com>
2025-08-24 13:53:53 +0300

    Add shutdown logic

--- webapp/src/nativeMain/kotlin/com/iv127/quizflow/webapp/Main.kt
+++ webapp/src/nativeMain/kotlin/com/iv127/quizflow/webapp/Main.kt
@@ -2,37 +2,34 @@ package com.iv127.quizflow.webapp
 
 import com.iv127.quizflow.core.startQuizFlowApplication
 import com.iv127.quizflow.webapp.file.FileIO
-import kotlin.concurrent.atomics.AtomicBoolean
-import kotlin.concurrent.atomics.ExperimentalAtomicApi
+import kotlinx.atomicfu.AtomicBoolean
+import kotlinx.atomicfu.atomic
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.staticCFunction
 import platform.posix.SIGINT
 import platform.posix.signal
 import platform.posix.sleep
 
+private val isShutdown: AtomicBoolean = atomic(false)
 
-@OptIn(ExperimentalAtomicApi::class)
+@OptIn(ExperimentalForeignApi::class)
 fun main(args: Array<String>) {
     val q = FileIO().readAll("/home/iv127/Projects/quiz-flow/webapp/src/nativeMain/resources/a.txt")
     println(byteArrayToStringUsingBuffer(q.first()))
 
     val serverApp = startQuizFlowApplication(args)
 
-    val isShutdown = AtomicBoolean(false)
-    registerSigintHandler {
-        serverApp.stop(5000, 10000)
-        isShutdown.store(true)
-    }
+    signal(SIGINT, staticCFunction { signal: Int ->
+        isShutdown.value = true
+    })
 
-    while (!isShutdown.load()) {
+    while (!isShutdown.value) {
         sleep(1U)
-        println(1)
     }
-
-    println(isShutdown)
+    serverApp.stop(10_000, 10_000)
 }
 
-fun byteArrayToStringUsingBuffer(byteArray: ByteArray): String {
+private fun byteArrayToStringUsingBuffer(byteArray: ByteArray): String {
     val stringBuilder = StringBuilder(byteArray.size)
     for (byte in byteArray) {
         stringBuilder.append(byte.toInt().toChar())
@@ -40,9 +37,3 @@ fun byteArrayToStringUsingBuffer(byteArray: ByteArray): String {
     return stringBuilder.toString()
 }
 
-@OptIn(ExperimentalForeignApi::class)
-private fun registerSigintHandler(func: () -> Unit) {
-    signal(SIGINT, staticCFunction { signal: Int ->
-        func()
-    })
-}
