3b88566b97beed059351e428428c338c727f9a85
IvanPostu <ipostu20000127@gmail.com>
2025-11-06 20:24:08 +0200

    chore: ui adjustments and add qa user

---
+++ quiz-flow-server/core/src/commonMain/resources/migrations/app/13__test_qa_user_Values.sql
@@ -0,0 +1,10 @@
+INSERT INTO "main"."users"
+("id", "created_at", "archived_at", "username", "password_hash", "json")
+VALUES
+('5a4dfdc6-019a-1000-0f06-00004db57797',
+'2025-11-06 17:54:12.038086213',
+NULL,
+'qa',
+'qa',
+'{"id":"5a4dfdc6-019a-1000-0f06-00004db57797","username":"qa","password_hash":"qa","created_date":"2025-11-06T17:54:12.038086213Z","archived_date":null}'
+);
