d735c24543c76ba176181b3477515c7c1e92b084
IvanPostu <ipostu20000127@gmail.com>
2025-12-05 00:36:54 +0200

    chore: test

--- quiz-flow-server/acceptance-test/src/test/java/com/iv127/quizflow/server/acceptance/test/route/ExampleJavaTest.java
+++ quiz-flow-server/acceptance-test/src/test/java/com/iv127/quizflow/server/acceptance/test/route/ExampleJavaTest.java
@@ -2,9 +2,14 @@ package com.iv127.quizflow.server.acceptance.test.route;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExampleJavaTest {
+
     @Test
     public void testEmptyTextBlock() {
         String abc = /*whitespaces ignored*/"""            
@@ -40,13 +45,40 @@ public class ExampleJavaTest {
     }
 
     @Test
-    public void dummyTest() {
-        assertThat(1 + 1)
-                .isEqualTo(2);
+    public void testExample() {
+        List<? extends Float> l1 = new ArrayList<Float>();
+        List<? super Float> l2 = new ArrayList<Float>();
+        assertThat(l1)
+                .isEqualTo(l2)
+                .isEmpty();
+    }
+
+    @Test
+    public void testSortStringsUsingNaturalOrderComparator() {
+        var list = new ArrayList<String>();
+        list.add("-100");
+        list.add("-200");
+        list.add("100");
+        list.add("200");
+        list.add("Aaaa");
+        list.add("aaaa");
+
+        list.sort(Comparator.naturalOrder());
+
+        assertThat(list)
+                .isEqualTo(List.of(
+                        "-100",
+                        "-200",
+                        "100",
+                        "200",
+                        "Aaaa",
+                        "aaaa"
+                ));
     }
 
     class InnerClassStaticField {
         private static final int QQQ = 99;
         private static int QQQ1 = 991;
     }
+
 }
